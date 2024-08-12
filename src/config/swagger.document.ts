42bf29118a6701f9baa7d6913ae4e52ae8e3e470
Jiwoong <dnd0311@naver.com>
2024-08-12 17:25:50 +0900

    swagger

---
+++ src/config/swagger.document.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Jiwoong')
+    .setDescription('5th practice')
+    .setVersion('1.0')
+    .addTag('Jiwoong practice')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api-docs', app, document);
+};
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,13 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProductDto {
+  @ApiProperty({ example: 'robot' })
   name: string;
 
+  @ApiProperty({ example: 'robot is better' })
   description: string;
 
+  @ApiProperty({ example: 'robota' })
   productImg: string;
 
+  @ApiProperty({ example: '1000' })
   price: number;
 
+  @ApiProperty({ example: '6' })
   stock: number;
 
+  @ApiProperty({ example: 'true' })
   isSale: boolean;
 }
