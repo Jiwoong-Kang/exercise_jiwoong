e7e6185a955483da255f62b5b3935929cfb8b3d1
Jiwoong <dnd0311@naver.com>
2024-09-06 21:19:53 -0600

    MINIO #1

---
+++ src/minio-client/file.model.ts
@@ -0,0 +1,26 @@
+export interface BufferedFile {
+  fieldName: string;
+  originalName: string;
+  encoding: string;
+  mimeType: AppMimeType;
+  size: number;
+  buffer: Buffer;
+}
+
+export interface StoredFile extends HasFile, StoredFileMetadate {}
+
+export interface HasFile {
+  file: Buffer | string;
+}
+
+export interface StoredFileMetadate {
+  id: string;
+  name: string;
+  encoding: string;
+  mimeType: AppMimeType;
+  size: number;
+  updatedAt: Date;
+  fileSrc?: string;
+}
+
+export type AppMimeType = 'image/png' | 'image/jpeg';
---
+++ src/minio-client/minio-client.service.ts
@@ -0,0 +1,81 @@
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { MinioService } from 'nestjs-minio-client';
+import { ConfigService } from '@nestjs/config';
+import { User } from '@user/entities/user.entity';
+import { BufferedFile } from '@minio-client/file.model';
+import * as crypto from 'crypto';
+
+@Injectable()
+export class MinioClientService {
+  private readonly logger: Logger;
+  private readonly baseBucket: string;
+
+  public get client() {
+    return this.minio.client;
+  }
+
+  constructor(
+    private readonly minio: MinioService,
+    private readonly configService: ConfigService,
+  ) {
+    this.logger = new Logger('MinioStorageService');
+    this.baseBucket = this.configService.get('MINIO_BUCKET');
+  }
+
+  public async uploadProfileImg(
+    user: User,
+    file: BufferedFile,
+    categoryName: string,
+    baseBucket: string = this.baseBucket,
+  ) {
+    if (!(file.mimeType.includes('jpeg') || file.mimeType.includes('png'))) {
+      throw new HttpException(
+        'Error uploading file type error',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const temp_filename = Date.now().toString();
+    const hashedFileName = crypto
+      .createHash('md5')
+      .update(temp_filename)
+      .digest('hex');
+
+    const ext = file.originalName.substring(
+      file.originalName.lastIndexOf('.'),
+      file.originalName.length,
+    );
+
+    const metaData = {
+      'Content-Type': file.mimeType,
+      'X-Amz-Meta_Testing': 1234,
+    };
+    const filename = hashedFileName + ext;
+    const fileBuffer = file.buffer;
+    const filePath = `${categoryName}/${user.id}/${filename}`;
+
+    if (`${categoryName}/${user.id}`.includes(user.id)) {
+      await this.deleteFolderContents(
+        this.baseBucket,
+        `${categoryName}/${user.id}/`,
+      );
+    }
+
+    this.client.putObject(
+      baseBucket,
+      filePath,
+      fileBuffer,
+      fileBuffer.length,
+      metaData,
+      function (err) {
+        console.log('================', err);
+        if (err) {
+          throw new HttpException(
+            'Error uploading processing error',
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+      },
+    );
+    return `http://${this.configService.get('MINIO_ENDPOINT')}:${this.configService.get('MINIO_PORT')}/${this.configService.get('MINIO_BUCKET')}/${filePath}`;
+  }
+}
